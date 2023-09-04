4fc2a28cc8e2878b785db76ee1a86453accf2c2c
ridhoakbar8977 <ridho8977@gmail.com>
2023-09-04 11:05:47 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import UserRoute from "./routes/UserRoute.js";
+const port = 5000;
+
+const app = express();
+
+// Connect MongoDB local at default port 27017.
+// mongodb://127.0.0.1:27017/contact_app
+
+const dbUrl = process.env.MONGODB_URI;
+mongoose.connect(dbUrl, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Database Connected..."));
+
+app.use(cors());
+app.use(express.json());
+app.use(UserRoute);
+
+app.get("/", (req, res) => {
+   res.send("Welcome to my server is express! new");
+});
+
+app.listen(port, () => console.log(`Server up and running in http://localhost:${port}.`));
---
+++ routes/UserRoute.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { getUsers, getUserById, saveUser, updateUser, deleteUser } from "./../controller/UserController.js";
+
+const router = express.Router();
+
+router.get("/users", getUsers);
+router.get("/users/:id", getUserById);
+router.post("/users", saveUser);
+router.patch("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
+
+export default router;
+
+// import express from "express";
+// import {
+//     getUsers,
+//     getUserById,
+//     saveUser,
+//     updateUser,
+//     deleteUser
+// } from "../controllers/UserController.js";
+
+// const router = express.Router();
+
+// router.get('/users', getUsers);
+// router.get('/users/:id', getUserById);
+// router.post('/users', saveUser);
+// router.patch('/users/:id', updateUser);
+// router.delete('/users/:id', deleteUser);
+
+// export default router;
